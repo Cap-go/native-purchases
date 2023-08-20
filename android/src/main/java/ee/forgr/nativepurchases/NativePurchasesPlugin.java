43d39bc96545c3e350ff39196aab5933ff8fab67
Martin Donadieu <martindonadieu@gmail.com>
2023-08-20 15:39:26 +0200

    fix: purchase flow android

--- android/src/main/java/ee/forgr/nativepurchases/NativePurchasesPlugin.java
+++ android/src/main/java/ee/forgr/nativepurchases/NativePurchasesPlugin.java
@@ -1,29 +1,31 @@
 package ee.forgr.nativepurchases;
 
 import android.util.Log;
-import androidx.annotation.NonNull;
 
 import com.android.billingclient.api.AcknowledgePurchaseParams;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.ProductDetails;
+import com.android.billingclient.api.ProductDetailsResponseListener;
 import com.android.billingclient.api.AcknowledgePurchaseResponseListener;
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingFlowParams;
 import com.android.billingclient.api.BillingResult;
 import com.android.billingclient.api.Purchase;
 import com.android.billingclient.api.PurchasesUpdatedListener;
-import com.android.billingclient.api.SkuDetails;
-import com.android.billingclient.api.SkuDetailsParams;
-import com.android.billingclient.api.SkuDetailsResponseListener;
+import com.android.billingclient.api.QueryProductDetailsParams;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableList;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,31 +34,9 @@ import org.json.JSONObject;
 public class NativePurchasesPlugin extends Plugin {
 
   public final String PLUGIN_VERSION = "2.0.13";
-
   private BillingClient billingClient;
-  private PluginCall purchaseCall;
 
-  @Override
-  public void load() {
-    super.load();
-      billingClient = BillingClient.newBuilder(getContext())
-          .setListener(new PurchasesUpdatedListener() {
-              @Override
-              public void onPurchasesUpdated(BillingResult billingResult, List<Purchase> purchases) {
-                  if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
-                      for (Purchase purchase : purchases) {
-                          handlePurchase(purchase);
-                      }
-                  } else {
-                      // Handle any other error codes.
-                  }
-              }
-          })
-          .enablePendingPurchases()
-          .build();
-  }
-
-  private void handlePurchase(Purchase purchase) {
+  private void handlePurchase(Purchase purchase, PluginCall purchaseCall) {
       if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED) {
           // Grant entitlement to the user, then acknowledge the purchase
           acknowledgePurchase(purchase.getPurchaseToken());
@@ -69,6 +49,10 @@ public class NativePurchasesPlugin extends Plugin {
           // purchase, and to complete it, they should follow instructions that are
           // given to them. You can also choose to remind the user to complete the
           // purchase if you detect that it is still pending.
+          purchaseCall.reject("Purchase is pending");
+      } else {
+          // Handle any other error codes.
+          purchaseCall.reject("Purchase is not purchased");
       }
   }
 
@@ -81,39 +65,98 @@ public class NativePurchasesPlugin extends Plugin {
           @Override
           public void onAcknowledgePurchaseResponse(BillingResult billingResult) {
               // Handle the result of the acknowledge purchase
+            Log.i("NativePurchases", "onAcknowledgePurchaseResponse" + billingResult);
           }
       });
   }
 
+    private void initBillingClient(PluginCall purchaseCall) {
+        CountDownLatch semaphoreReady = new CountDownLatch(1);
+        billingClient = BillingClient.newBuilder(getContext())
+                .setListener(new PurchasesUpdatedListener() {
+                    @Override
+                    public void onPurchasesUpdated(BillingResult billingResult, List<Purchase> purchases) {
+                        Log.i("NativePurchases", "onPurchasesUpdated" + billingResult);
+                        if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
+                          for (Purchase purchase : purchases) {
+                              handlePurchase(purchase, purchaseCall);
+                          }
+                        } else {
+                          // Handle any other error codes.
+                            Log.i("NativePurchases", "onPurchasesUpdated" + billingResult);
+                            purchaseCall.reject("Purchase is not purchased");
+                        }
+                        return;
+                    }})
+                .enablePendingPurchases()
+                .build();
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if (billingResult.getResponseCode() ==  BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                    semaphoreReady.countDown();
+                }
+            }
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
+        try {
+            semaphoreReady.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
   @PluginMethod
   public void purchaseProduct(PluginCall call) {
-      String productIdentifier = call.getString("productIdentifier");
-      Log.d("CapacitorPurchases", "purchaseProduct: " + productIdentifier);
-      purchaseCall = call;
+        String productIdentifier = call.getString("productIdentifier");
+// cannot use quantity, because it's done in native modal
+        Log.d("CapacitorPurchases", "purchaseProduct: " + productIdentifier);
+        if (productIdentifier.isEmpty()) {
+            // Handle error: productIdentifier is empty
+            call.reject("productIdentifier is empty");
+            return;
+        }
+        ImmutableList<QueryProductDetailsParams.Product> productList = ImmutableList.of(QueryProductDetailsParams.Product.newBuilder()
+                .setProductId(productIdentifier)
+                .setProductType(BillingClient.ProductType.INAPP)
+                .build());
+        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
+                .setProductList(productList)
+                .build();
+        this.initBillingClient(call);
+        billingClient.queryProductDetailsAsync(
+                params,
+                new ProductDetailsResponseListener() {
+                    public void onProductDetailsResponse(BillingResult billingResult, List<ProductDetails> productDetailsList) {
+                        Log.i("NativePurchases", "onProductDetailsResponse" + billingResult + productDetailsList);
+                        // Process the result
+                        ProductDetails productDetails = productDetailsList.get(0);
+                        ImmutableList<BillingFlowParams.ProductDetailsParams> productDetailsParamsList =
+                                ImmutableList.of(
+                                        BillingFlowParams.ProductDetailsParams.newBuilder()
+                                                .setProductDetails(productDetails)
+                                                .build()
+                                );
 
-      if (billingClient.isReady()) {
-          List<String> skuList = new ArrayList<>();
-          skuList.add(productIdentifier);
-          SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
-          params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+                        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                                .setProductDetailsParamsList(productDetailsParamsList)
+                                .build();
 
-          billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
-              @Override
-              public void onSkuDetailsResponse(BillingResult billingResult, List<SkuDetails> skuDetailsList) {
-                  if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && skuDetailsList != null) {
-                      for (SkuDetails skuDetails : skuDetailsList) {
-                          BillingFlowParams flowParams = BillingFlowParams.newBuilder()
-                                  .setSkuDetails(skuDetails)
-                                  .build();
-                          billingClient.launchBillingFlow(getActivity(), flowParams);
-                      }
-                  }
-              }
-          });
-      }
-  }
+                        // Launch the billing flow
+                        BillingResult billingResult2 = billingClient.launchBillingFlow(getActivity(), billingFlowParams);
+                        Log.i("NativePurchases", "onProductDetailsResponse2" + billingResult2);
+                    }
+                }
+        );
+    }
 
-  @PluginMethod
+
+
+    @PluginMethod
   public void restorePurchases(PluginCall call) {
       Log.d("NativePurchases", "restorePurchases");
 
@@ -158,38 +201,59 @@ public void getProducts(PluginCall call) {
     }
 
     Log.d("NativePurchases", "getProducts: " + productIdentifiers);
+// Retrieve a value for "productDetails" by calling queryProductDetailsAsync()
+// Get the offerToken of the selected offer
+//    String offerToken = productDetails
+//            .getSubscriptionOfferDetails()
+//            .get(selectedOfferIndex)
+//            .getOfferToken();
+// Set the parameters for the offer that will be presented
+// in the billing flow creating separate productDetailsParamsList variable
+//    ImmutableList<ProductDetailsParams> productDetailsParamsList =
+//            ImmutableList.of(
+//                    BillingFlowParams.ProductDetailsParams.newBuilder()
+//                            .setProductDetails(p)
+//                            .setOfferToken(offerToken)
+//                            .build()
+//            );
+//
+//    BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+//            .setProductDetailsParamsList(productDetailsParamsList)
+//            .build();
+//
+//// Launch the billing flow
+//    BillingResult billingResult = billingClient.launchBillingFlow(activity, billingFlowParams);
 
-    if (billingClient.isReady()) {
-        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
-        params.setSkusList(productIdentifiers).setType(BillingClient.SkuType.INAPP);
-
-        billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
-            @Override
-            public void onSkuDetailsResponse(BillingResult billingResult, List<SkuDetails> skuDetailsList) {
-                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && skuDetailsList != null) {
-                    JSObject ret = new JSObject();
-                    JSONArray products = new JSONArray();
-
-                    for (SkuDetails skuDetails : skuDetailsList) {
-                        JSObject product = new JSObject();
-                        product.put("productId", skuDetails.getSku());
-                        product.put("title", skuDetails.getTitle());
-                        product.put("description", skuDetails.getDescription());
-                        product.put("price", skuDetails.getPrice());
-                        product.put("priceAmountMicros", skuDetails.getPriceAmountMicros());
-                        product.put("priceCurrencyCode", skuDetails.getPriceCurrencyCode());
-                        product.put("type", skuDetails.getType());
-                        products.put(product);
-                    }
-
-                    ret.put("products", products);
-                    call.resolve(ret);
-                } else {
-                    // Handle any other error codes.
-                }
-            }
-        });
-    }
+//        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+//        params.setSkusList(productIdentifiers).setType(BillingClient.SkuType.INAPP);
+//
+//        billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
+//            @Override
+//            public void onSkuDetailsResponse(BillingResult billingResult, List<SkuDetails> skuDetailsList) {
+//                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && skuDetailsList != null) {
+//                    JSObject ret = new JSObject();
+//                    JSONArray products = new JSONArray();
+//
+//                    for (SkuDetails skuDetails : skuDetailsList) {
+//                        JSObject product = new JSObject();
+//                        product.put("productId", skuDetails.getSku());
+//                        product.put("title", skuDetails.getTitle());
+//                        product.put("description", skuDetails.getDescription());
+//                        product.put("price", skuDetails.getPrice());
+//                        product.put("priceAmountMicros", skuDetails.getPriceAmountMicros());
+//                        product.put("priceCurrencyCode", skuDetails.getPriceCurrencyCode());
+//                        product.put("type", skuDetails.getType());
+//                        products.put(product);
+//                    }
+//
+//                    ret.put("products", products);
+//                    call.resolve(ret);
+//                } else {
+//                    // Handle any other error codes.
+//                }
+//            }
+//        });
+//    }
 }
 
   //================================================================================
